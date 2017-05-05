73340185c7d4e0e4ee63d07b3d617162213dc183
amriteya <amriteyapandey@gmail.com>
2017-05-06 01:01:50 +0530

    Altered response type and hierarchy of controllers

--- src/main/java/com/spicinemas/controller/SignUpController.java
+++ src/main/java/com/spicinemas/controller/SignUpController.java
@@ -1,28 +1,39 @@
 package com.spicinemas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spicinemas.model.Token;
 import com.spicinemas.model.User;
 import com.spicinemas.repository.UserRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-@RequestMapping("/v1/api")
-public class SignUpController {
-	
+@Slf4j
+public class SignUpController implements BaseController {
+
 	@Autowired
 	UserRepository userRepo;
 
-	@PostMapping(value="/signup", consumes=MediaType.APPLICATION_JSON_VALUE)
-	public String signup(@RequestBody Token token){
+	@PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> signup(@RequestBody User user) {
+
+		log.info("Requesting resource /v1/api/signup");
 		
-		User u = new User("Amr", "slkdj");
-		userRepo.save(u);
-		return "HEllo WOrld";
+		try {
+			userRepo.save(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return new ResponseEntity<String>("Failed", HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<String>("Success", HttpStatus.ACCEPTED);
 	}
+	
 }
