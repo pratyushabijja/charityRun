34f31bd2b0d52aa8f795fb6f9ef0529e4809ae56
amriteya <amriteyapandey@gmail.com>
2017-05-06 01:01:04 +0530

    Removed unrequired method signatures

--- src/main/java/com/spicinemas/repository/UserRepository.java
+++ src/main/java/com/spicinemas/repository/UserRepository.java
@@ -7,9 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.spicinemas.model.User;
 
 public interface UserRepository extends MongoRepository<User, Long>{
-	
-	public User findByFirstName(String firstName);
-	
-    public List<User> findByLastName(String lastName);
     
 }
