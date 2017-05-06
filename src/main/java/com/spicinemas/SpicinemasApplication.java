390ce9c24575063a8a88f1e76939379a5b2643a4
amriteya <amriteyapandey@gmail.com>
2017-05-06 18:22:43 +0530

    notification util

--- src/main/java/com/spicinemas/SpicinemasApplication.java
+++ src/main/java/com/spicinemas/SpicinemasApplication.java
@@ -1,12 +1,26 @@
 package com.spicinemas;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.spicinemas.notification.NotificationUtil;
 
 @SpringBootApplication
 public class SpicinemasApplication {
+	
+	@Autowired
+	NotificationUtil notifUtil;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpicinemasApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> notifUtil.sendDummyNotification();
 	}
 }
